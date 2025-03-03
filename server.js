b14c19786a49f9b8c6d6783e823b3ce467f4f3ab
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-03 11:15:47 -0800

    Use fs to print to a new file

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const validator = require("validator");
+const fs = require("fs");
+
+fs.writeFile("./hello.txt", "hello, friend", () => {
+  console.log("done creating file");
+});
 
 const test = "absoidfve";
 console.log(validator.isAlpha(test));
